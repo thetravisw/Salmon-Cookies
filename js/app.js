1d8351ed912d33c8157d1aaeec335b495d1053c0
Travis Williams <travisjw@gmail.com>
2018-05-08 23:21:13 -0700

    streeeeetch goaaaaaaaals

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 
 //   Declare Global Variables =======================================
-var timeArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'], visitors = [], purchases = [];
+var timeArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'], visitors = [], purchases = [], EmployeesNeeded = [];
 var salesTable = document.getElementById('salesTable');
-
+var yogaTable = document.getElementById('EmployeesNeeded');
 //   Build the Constructor Function  ===============================
 function Store(location, minimumHourlyCustomers, maximumHourlyCustomers, averageCustomerSales) {
     this.location = location;
@@ -24,6 +24,16 @@ Store.prototype.populateSalesAndRender = function () {
     newTd.textContent = '' + this.location;
     newTr.appendChild(newTd);
 
+    //Yoga Pants Seeding  ===  'Cause they so Streeeetchy
+    //Also, I'm hencefort using Yoga to indicate strecth goal work
+    //for this project.
+
+    var trYoga = document.createElement('tr');
+    var tdYoga = document.createElement('td');
+    tdYoga.textContent = '' + this.location;
+    trYoga.appendChild(tdYoga);
+
+    //build Table Row in for loop (mostly)
     for (var i in timeArray) {
         //  Calculate Visitors and Sales, and Populate Arrays
         visitors[i] = Math.floor(Math.random() * (this.maximumHourlyCustomers - this.minimumHourlyCustomers) + 1 + this.minimumHourlyCustomers);
@@ -32,30 +42,68 @@ Store.prototype.populateSalesAndRender = function () {
 
         totalSales = purchases[i] + totalSales;
 
+        //  Yoga:  Calculate Employees needed  [visitors/20, minimum 2]
+        EmployeesNeeded[i] = Math.ceil(visitors[i]/20 );
+        if (EmployeesNeeded[i] < 2){EmployeesNeeded[i]=2;}
+        
         //  Render in the Table
 
         newTd = document.createElement('td');
         newTd.textContent = '$' + purchases[i];
         newTr.appendChild(newTd);  
+
+        //Yoga:  Render into employees needed table
+
+        tdYoga = document.createElement('td');
+        tdYoga.textContent = ''+ EmployeesNeeded[i];
+        trYoga.appendChild(tdYoga); 
     }
-    salesTable.appendChild(newTr)
+
+    // total sales
+    newTd = document.createElement('td');
+    newTd.textContent = '$' + Math.floor(totalSales*100)/100;
+    newTr.appendChild(newTd);
+
+    //append completed Tr to table
+    salesTable.appendChild(newTr);
+    yogaTable.appendChild(trYoga);
 }
 
 //  Build Table header  ================================================
 
 var newTr = document.createElement('tr');
 var newTh = document.createElement('th');
-newTh.textContent = '';
+newTh.textContent = 'Location';
 newTr.appendChild(newTh);
 
+//  Yoga Pants Header
+var trYoga = document.createElement('tr');
+var thYoga = document.createElement('th');
+thYoga.textContent = 'Location';
+trYoga.appendChild(thYoga);
+
+
 for (var i in timeArray) {
+    //create Header elements
     newTh = document.createElement('th');
     newTh.textContent = '' + timeArray[i];
     newTr.appendChild(newTh);
+
+    //  Headers for Yoga Pants
+    thYoga = document.createElement('th');
+    thYoga.textContent = '' + timeArray[i];
+    trYoga.appendChild(thYoga);
 }
+
+//Daily Sales Header
+newTh = document.createElement('th');
+newTh.textContent='Daily Sales';
+newTr.appendChild(newTh);
 
 // append the tr to the table
 salesTable.appendChild(newTr);
+yogaTable.appendChild(trYoga);
+
 
 //  Call the constructor function   ===============================
 var firstAndPike = new Store('First And Pike', 23, 65, 6.3);
@@ -64,7 +112,6 @@ var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capHill = new Store('Cap Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
-
 //   Party cause it's done... or Cry in beer cause it's Broken  ====
 
 firstAndPike.populateSalesAndRender();
@@ -72,3 +119,7 @@ seaTac.populateSalesAndRender();
 seattleCenter.populateSalesAndRender();
 capHill.populateSalesAndRender();
 alki.populateSalesAndRender();
+
+
+
+//Do the stretchy Yoga pants thang.
